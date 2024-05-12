34d6c6edf0f8d378c3614a7336c838dfa91eec44
sl3meyy <sl3mey@gmail.com>
2024-05-12 17:26:19 +0200

    Changed by sl3mey

--- main.py
+++ main.py
@@ -1,62 +1,78 @@
-from typing import Final
 import os
-from discord.app_commands import commands
-from discord import Intents, Client, Message
-from random import choice, randint
-from functions import *
-f = open("token.txt", "r")
-token = f.read().replace("BOT_TOKEN=", "")
-#print(token)
+import json
+from discord import Intents, Client, Message, Role
 
-#Setting up Bot
+# Lese den Token aus der Datei
+with open("token.txt", "r") as f:
+    token = f.read().strip()
 
-intents: Intents = Intents.default()
-intents.message_content = True # NOQA
-client: Client = Client(intents=intents)
-bot = commands.Bot(command_prefix='$', intents=intents)
-#Message functions
+# Lese die Konfiguration aus der JSON-Datei
+with open("iris.json", "r") as f:
+    config = json.load(f)
 
-async def send_message(message: Message, user_message: str) -> None:
-    if not user_message:
-        print('Messages was empty because intents were not enabled probably')
-        return
+# Extrahiere die Konfigurationsdaten
+orga_kasse = config.get("orgaKasse", 100)
+team_members = config.get("teamMembers", [])
 
-    if is_private := user_message[0] == '?':
-        user_message = user_message[1:]
+# Erstelle den Discord-Client
+intents = Intents.default()
+intents.message_content = True
+client = Client(intents=intents)
 
-    try:
-        response: str = get_response(user_message)
-        await message.author.send(response) if is_private else await message.channel.send(response)
-    except Exception as e:
-        print(e)
+# Definiere die ID des Channels
+channel_id = 123456789012345678  # Hier die tatsächliche Channel-ID eintragen
 
-#Startup
+# Antwortnachricht für /test
+response_message_test = "Everything is fine"
+# Antwortnachricht für /clear
+response_message_clear = "Channel cleared!"
+# Antwortnachricht für /balance
+response_message_balance = f"OrgaKasse: {orga_kasse}"
+
+# Rollen IDs
+role_ids = [123456789012345678,  # Hier die tatsächlichen Rollen-IDs eintragen
+            234567890123456789,
+            345678901234567890]
+
+# Mapping von Befehlen zu erlaubten Rollen
+command_roles = {
+    '/clear': [role_ids[0]],
+    '/balance': role_ids
+}
+
+# Event für den Bot-Start
 @client.event
-async def on_ready() -> None:
+async def on_ready():
     print(f'{client.user} is now running!')
 
-#Handle incomming messages
-
+# Event für eingehende Nachrichten
 @client.event
-async def on_message(message: Message) -> None:
-    if message.author == client.user:
-        return
+async def on_message(message: Message):
+    # Überprüfe, ob die Nachricht im gewünschten Channel ist
+    if message.channel.id == channel_id:
+        if message.content.startswith('/test'):
+            await message.channel.send(response_message_test)
+        elif message.content.startswith('/clear'):
+            await clear_channel(message.channel, message.author.roles)
+        elif message.content.startswith('/balance'):
+            await message.channel.send(response_message_balance)
+    # Überprüfe, ob die Nachricht eine private Nachricht an den Bot ist
+    elif isinstance(message.channel, discord.DMChannel):
+        if message.content.startswith('/test'):
+            await message.author.send(response_message_test)
 
-    username: str = str(message.author)
-    user_message: str = str(message.content)
-    channel: str = str(message.channel)
-    print(f'[{channel}] {username}: ' +user_message)
-    await send_message(message, user_message)
+# Funktion zum Löschen des Channels
+async def clear_channel(channel, user_roles):
+    allowed_roles = command_roles['/clear']
+    if any(role.id in allowed_roles for role in user_roles):
+        await channel.purge()
+        await channel.send(response_message_clear)
+    else:
+        await channel.send("You do not have permission to execute this command.")
 
-
-#main entry point
-
-def main() -> None:
-    client.run(token=token)
+# Main-Funktion
+def main():
+    client.run(token)
 
 if __name__ == '__main__':
     main()
-
-
-
-
